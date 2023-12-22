04defa67feca2d64f3e4096ac7ba59b7be2861a8
ericlin2 <ericlin45209@gmail.com>
2023-12-22 16:32:36 +0800

    implemented router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import {Outlet} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header'; 
 import Footer from './components/Footer';
+
 
 
 const App = () => {
@@ -11,7 +13,7 @@ const App = () => {
       <Header />
       <main className='py-3'> 
         <Container>
-          <h1>Welcome To FruitShop</h1>
+          <Outlet />
         </Container>
       </main>
 
